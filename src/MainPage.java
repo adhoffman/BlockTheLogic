089d9f7be2237d8ac7c02a4b91f23fe3e1d5dcb2
Alex Hoffman <lexbravarymusic@gmail.com>
2017-04-12 23:23:52 -0400

    Add Prospect tab that will create a new prospect client in database.

--- src/MainPage.java
+++ src/MainPage.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
  */
 public class MainPage extends JFrame{
     private JButton runQueryButton;
-    private JTextField textField1;
+    private JTextField firstNameField;
     private MySQLConnector connector;
     private JPanel rootPane;
     private JTabbedPane tabbedPane1;
@@ -24,6 +24,13 @@ public class MainPage extends JFrame{
     private JPanel CompleteTab;
     private JPanel ClientsTab;
     private JPanel ReportsTab;
+    private JButton addProspectButton;
+    private JTextField facebookNameField;
+    private JTextField lastNameField;
+    private JTextField artistgroupNameField;
+    private JTextField emailField;
+    private JTextField websitesField;
+    private JTextField referencesField;
 
     public MainPage(MySQLConnector connector) {
         super("BlockTheLogic");
@@ -34,6 +41,19 @@ public class MainPage extends JFrame{
         setContentPane(rootPane);
 
         setVisible(true);
+
+
+        addProspectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    connector.addProspect(firstNameField.getText(),lastNameField.getText(),artistgroupNameField.getText(),emailField.getText(),facebookNameField.getText(),websitesField.getText(), referencesField.getText());
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+        });
     }
 
 
--- src/MySQLConnector.java
+++ src/MySQLConnector.java
@@ -1,31 +1,28 @@
 import java.sql.*;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by alexhoffman on 1/28/17.
  */
 public class MySQLConnector {
+    private Connection connection;
+    private Statement statement;
 
-    public void MySQLConnector (){
-
+    public void MySQLConnector() throws SQLException {
+         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TEST_LOGICBLOCKSTUDIOS", "student", "student");
+         statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
     public void queryProject() throws SQLException {
 
-        // 1. Get a connection to database
-        Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TEST_LOGICBLOCKSTUDIOS", "student", "student");
-
-
-        // 2. Create a Statement
-        Statement myStatement = null;
-        try {
-            myStatement = myConnection.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
         // 3. Execute SQL Query
-        ResultSet result = myStatement.executeQuery("SELECT * FROM PROJECT");
+        ResultSet result = statement.executeQuery("SELECT * FROM PROJECT");
         // 4. Process the result set
 
         while (result.next()) {
@@ -34,28 +31,22 @@ public class MySQLConnector {
 
     }
 
-    public void queryProject(String query) throws SQLException {
 
-        // 1. Get a connection to database
-        Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TEST_LOGICBLOCKSTUDIOS", "student", "student");
+    public void addProspect(String firstName, String lastName, String artistgroupName, String email, String facebookName, String website, String references) throws SQLException {
 
+        String today = new SimpleDateFormat("yyyy-MM-dd").format(new java.util.Date());
+        String query = "INSERT INTO CONTACT (FIRST_NAME,LAST_NAME,ARTIST_GROUP_NAME,EMAIL,FACEBOOK_NAME,WEBSITES,REFERENCE_CREDIT,CREATE_DATE,LAST_CONTACT_DATE,MAILING_LIST_PERM) VALUES (\""+firstName+"\",\""+lastName+"\",\""+artistgroupName+"\",\""+email+"\",\""+facebookName+"\",\""+website+"\",100,\""+today+"\",\""+today+"\",\"N\");";
 
-        // 2. Create a Statement
-        Statement myStatement = null;
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TEST_LOGICBLOCKSTUDIOS", "student", "student");
+        statement = null;
         try {
-            myStatement = myConnection.createStatement();
+            statement = connection.createStatement();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        // 3. Execute SQL Query
-        ResultSet result = myStatement.executeQuery(query);
-        // 4. Process the result set
-
-        while (result.next()) {
-            System.out.println(result.getString("PROJECT_TITLE") + " , " + result.getString("SONG_COUNT"));
-        }
+        statement.execute(query);
 
     }
-
 }
+
